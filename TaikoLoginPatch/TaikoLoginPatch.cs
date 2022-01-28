63b09028bc3466880c1af164f6c831fa32d632c9
brent <brent.bloxam@gmail.com>
2022-01-28 01:07:17 -0500

    Add project files.

---
+++ TaikoLoginPatch/TaikoLoginPatch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using Microsoft.Xbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using XGamingRuntime;
+using BepInEx;
+
+namespace TaikoLoginPatch
+{
+    [BepInPlugin("org.bepinex.plugins.taikologinpatch", "TaikoLoginPatch", "1.0.0.0")]
+    public class TaikoLoginPatch : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("Loaded");
+
+            // Load patches
+            Harmony.CreateAndPatchAll(typeof(TaikoLoginPatch));
+            Logger.LogInfo("Patched all");
+        }
+
+        [HarmonyPatch(typeof(SDK), "XUserAddAsync")]
+        [HarmonyPrefix]
+        private static void SDK_XUserAddAsync_Prefix(ref XUserAddOptions options, XUserAddCompleted completionRoutine)
+        {
+            options = XUserAddOptions.AddDefaultUserAllowingUI;
+        }
+    }
+}
